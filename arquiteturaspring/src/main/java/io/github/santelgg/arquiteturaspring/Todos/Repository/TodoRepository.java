c757336983b5eafecfd4053cca8860887af50366
luisguilherme <l.g.g.sante@gmail.com>
2025-09-09 12:59:37 -0300

    criação do packge Todos (To-Do) e criação de entity, repository e service

---
+++ arquiteturaspring/src/main/java/io/github/santelgg/arquiteturaspring/Todos/Repository/TodoRepository.java
@@ -0,0 +1,12 @@
+package io.github.santelgg.arquiteturaspring.Todos.Repository;
+
+import io.github.santelgg.arquiteturaspring.Todos.Entity.TodoEntity;
+import io.github.santelgg.arquiteturaspring.Todos.Service.TodoService;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, Integer> {
+
+
+}
---
+++ arquiteturaspring/src/main/java/io/github/santelgg/arquiteturaspring/Todos/Service/TodoService.java
@@ -0,0 +1,20 @@
+package io.github.santelgg.arquiteturaspring.Todos.Service;
+
+import io.github.santelgg.arquiteturaspring.Todos.Entity.TodoEntity;
+import io.github.santelgg.arquiteturaspring.Todos.Repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    private TodoRepository repository;
+
+    public TodoService(TodoRepository todoRepository){
+        this.repository = todoRepository;
+    }
+
+    public TodoEntity salvar(TodoEntity novoTodo){
+        return repository.save(novoTodo);
+    }
+}
